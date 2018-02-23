1bf860827aeb3ae86d441e55f89687a0aa4b0fa7
J.D. Bean <jonathandbean@gmail.com>
2018-02-23 16:59:26 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,7 @@ def consolidate_cart(cart)
       new_hash[item] ||= []
       data_hash.each do |key, value|
         new_hash[key] = value
-        new_hash[:count] ||= []
+        new_hash[:count] ||= 0
         new_hash[:count] += 1
     end
   end
